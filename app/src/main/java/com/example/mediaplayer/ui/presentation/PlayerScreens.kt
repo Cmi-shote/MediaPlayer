2c2034da38a7faf1affb369b77a51bb76632698e
Cmi-shote <similoluwashote@gmail.com>
2025-11-18 22:59:46 +0100

    #fix- update mini video player ui

--- app/src/main/java/com/example/mediaplayer/ui/presentation/PlayerScreens.kt
+++ app/src/main/java/com/example/mediaplayer/ui/presentation/PlayerScreens.kt
@@ -8,9 +8,11 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 
@@ -20,16 +22,16 @@ fun MiniPlayerScreen(
     onExpandClick: () -> Unit
 ) {
     Column(
-        modifier = Modifier.padding(16.dp),
-        verticalArrangement = Arrangement.Center
+        modifier = Modifier.fillMaxSize().padding(16.dp),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
     ) {
         VideoPlayer(
             player = viewModel.player,
             shouldShowFullScreenButton = true,
             onExpandVideoClick = onExpandClick,
             modifier = Modifier
-                .height(200.dp)
-                .fillMaxWidth()
+                .size(height = 200.dp, width = 250.dp)
         )
     }
 }
